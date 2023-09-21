4217303a511e1b7589791dadb36e323e5f74081c
uzner1989 <144073717+uzner1989@users.noreply.github.com>
2023-09-20 22:51:28 -0300

    Delete pharmacy_depot directory

--- pharmacy_depot/inventario/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from inventario import views
-
-urlpatterns = [
-    path('productos/', views.producto_list, name='producto_list'),
-    path('productos/<int:producto_id>/', views.producto_detail, name='producto_detail'),
-]
